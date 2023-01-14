3204d9f0855202d1446bc46e72c062701bfe09ae
grnisna <mdnisanahmed63@gmail.com>
2023-01-14 20:03:51 +0600

    responsive done for about secotn

--- src/FixedComponent/HomePage/AboutMe.js
+++ src/FixedComponent/HomePage/AboutMe.js
@@ -6,14 +6,14 @@ import computerProjects from "../../Assets/computerProjects.png";
 import teamProjects from "../../Assets/teamProjects.png";
 const AboutMe = () => {
   return (
-    <div className="flex justify-items-center">
+    <div className="lg:flex justify-items-center lg:mt-0 mt-5">
       {/* --------------------photo area --------------- */}
       <div
         style={{
           background: `url(${AboutMeBG})`,
           backgroundSize: "cover",
         }}
-        className="photoArea lg:w-[30%] ml-14 p-5"
+        className="photoArea lg:w-[30%] lg:ml-14 lg:p-5 "
       >
         <div className="grid grid-cols-2 gap-2">
           <img
@@ -42,13 +42,13 @@ const AboutMe = () => {
       {/* --------------------text area --------------- */}
 
       <div className="textAria lg:w-[70%]">
-        <h1 className="text-5xl font-bold font-mono ml-10">
+        <h1 className="lg:text-5xl text-3xl font-bold font-mono lg:ml-10 lg:px-0 px-5 lg:mt-0 mt-5">
           ABOUT ME <AiOutlineMinus size={36} />{" "}
         </h1>
-        <h2 className="text-xl font-bold font-sans ml-10 ">
+        <h2 className="text-xl font-bold font-sans lg:ml-10  px-5 lg:px-0">
           I'm Front-End Developer with over 1.5 years of experience
         </h2>
-        <h2 className="text-xl font-sans ml-5 text-justify px-5 ">
+        <h2 className="text-xl font-sans lg:ml-5 text-justify lg:px-5 px-5">
           As a enthusiast, I explore new thing. Base on my interst, i explore
           programming language. I really like JavaScirpt. It's easy to learn and
           has library, framework that cab be done big big web application. I
@@ -58,7 +58,7 @@ const AboutMe = () => {
           Freelancer in different Market-place like "Fiverr, Upwork, People Per
           Hours, Freeelancer.com ".
         </h2>
-        <div className="flex justify-center items-center ml-10">
+        <div className="flex justify-center items-center lg:ml-10 px-5">
           <div className="mx-auto ">
             <img className=" p-5" src={computerProjects} alt="" />
             <h3 className="font-bold">20+ Projects</h3>
