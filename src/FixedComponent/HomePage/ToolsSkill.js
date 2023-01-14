26c4cbd680a1f4167270b3adc0bf897b0e35e719
grnisna <mdnisanahmed63@gmail.com>
2023-01-15 00:08:01 +0600

    skills added

--- src/FixedComponent/HomePage/ToolsSkill.js
+++ src/FixedComponent/HomePage/ToolsSkill.js
@@ -14,7 +14,7 @@ import microsoft from "../../Assets/Icons/toolsSkill/microsoft.png"
 
 const ToolsSkill = () => {
   return (
-    <div className="lg:flex justify-items-center lg:flex-row-reverse lg:mt-28 mt-5">
+    <div className="lg:flex justify-items-center lg:mt-28 mt-5">
       <div>
         <h1 className="lg:text-5xl text-3xl font-bold font-mono lg:ml-10 lg:px-0 px-5 lg:mt-0 mt-5">
           TOOL'S SKILLS
