672ad994eba57e738baa66ea5a6e4aafafa93a2f
grnisna <mdnisanahmed63@gmail.com>
2023-02-02 10:33:16 +0600

    firebase deploy

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChotv5AuoDLRRLhw4ydtgudNTje0RrYa0",
+  authDomain: "golam-rasul.firebaseapp.com",
+  projectId: "golam-rasul",
+  storageBucket: "golam-rasul.appspot.com",
+  messagingSenderId: "470328328953",
+  appId: "1:470328328953:web:2d29b16e2a48998f245828"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
